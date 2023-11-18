ebc7d8bb2a737434f14e61a7665d0cd3a5457fcc
Guilhermemot <guilhermemapurunga13@gmail.com>
2023-11-18 15:03:40 -0300

    Aula dia 18/11 objetos

---
+++ Aula18-11-23/script.js
@@ -0,0 +1,13 @@
+let carro = {
+    marca: "Fiat",
+    preco: 28.789,
+    modelo: "Mobi"
+}
+let pessoa = {
+    nome: "Guilherme",
+    idade: 23,
+    tamanho: 1.78,
+    rua: "Avenida visconde do rio branco"
+}
+
+alert("Boa tarde " + pessoa.nome + " sua idade é " + pessoa.idade + " você possui " + pessoa.tamanho + " metros e mora na " + pessoa.rua);
